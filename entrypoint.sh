2d4df59d273afeb97643df576888fb80bb921f04
antoine savajols <antoine.savajols@gmail.com>
2021-12-15 18:09:43 +0100

    added file change update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,20 @@
 
 # This entrypoint convert pres.adoc into pres.html
 # It clone locally the reveal.js project in order to have speaker notes working
+# It watches file changes and reconvert the presentation on save using inotify
 # Finally it starts a light http server using ruby and httpd
-
-asciidoctor-revealjs pres.adoc
 
 if [ ! -d slides/reveal.js ]; then
     git clone https://github.com/hakimel/reveal.js.git --branch=3.9.2 --depth 1
 fi;
 
-ruby -run -e httpd . -p 8080
+asciidoctor-revealjs pres.adoc
+ruby -run -e httpd . -p 8080 &
+
+while inotifywait -e close_write pres.adoc
+do
+    echo "file changed triggered"
+    asciidoctor-revealjs pres.adoc
+    kill -9 $(pidof ruby)
+    ruby -run -e httpd . -p 8080 &
+done
